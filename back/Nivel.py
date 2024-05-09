79e69ece1d95329b60a4a5903326169fcef9f804
mhrrosa <71407848+mhrrosa@users.noreply.github.com>
2024-05-08 21:34:32 -0300

    enviando

--- back/Nivel.py
+++ back/Nivel.py
@@ -3,19 +3,19 @@ class Nivel:
         self.db = db
 
     def add_nivel(self, nivel):
-        self.db.cursor.execute("INSERT INTO nivel_maturidade_mpsbr (nivel) VALUES (%s)", (nivel,))
+        self.db.cursor.execute("INSERT INTO nivel_maturidade_mpsbr (Nivel) VALUES (%s)", (nivel,))
         self.db.conn.commit()
 
     def get_all_niveis_ordered(self):
-        self.db.cursor.execute("SELECT * FROM nivel_maturidade_mpsbr ORDER BY nivel")
+        self.db.cursor.execute("SELECT * FROM nivel_maturidade_mpsbr ORDER BY Nivel")
         result = self.db.cursor.fetchall()
         return result
 
     def delete_nivel(self, nivel_id):
-        self.db.cursor.execute("DELETE FROM nivel_maturidade_mpsbr WHERE id = %s", (nivel_id,))
+        self.db.cursor.execute("DELETE FROM nivel_maturidade_mpsbr WHERE ID = %s", (nivel_id,))
         self.db.conn.commit()
 
     def update_nivel(self, nivel_id, novo_nivel):
         novo_nivel_aux = novo_nivel['nivel']
-        self.db.cursor.execute("UPDATE nivel_maturidade_mpsbr SET nivel = %s WHERE id = %s", (novo_nivel_aux, nivel_id))
+        self.db.cursor.execute("UPDATE nivel_maturidade_mpsbr SET Nivel = %s WHERE ID = %s", (novo_nivel_aux, nivel_id))
         self.db.conn.commit()
