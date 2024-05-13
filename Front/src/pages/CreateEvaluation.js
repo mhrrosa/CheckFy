a971c98bbe307d9d72f679df8964d9c95f8182de
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2024-05-13 19:31:02 -0300

    css update of Niveis and CreateEvaluation

--- Front/src/pages/CreateEvaluation.js
+++ Front/src/pages/CreateEvaluation.js
@@ -36,7 +36,7 @@ function CreateEvaluation() {
   return (
     <div className="create-evaluation-container">
       <div className="form-section">
-      <button className="close-button">
+      <button className="close-button" onClick={() => navigate('/')}>
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <line x1="18" y1="6" x2="6" y2="18"></line>
           <line x1="6" y1="6" x2="18" y2="18"></line>
