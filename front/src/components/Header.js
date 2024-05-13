6b6ee4a361d749896a617fc605ad6bf86fd1239e
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2024-05-13 16:45:09 -0300

    css update of header, home and CreateEvaluation

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Header.css'; // Importe o CSS para estilização
+import '../styles/Header.css';
+import logo from '../img/logo_horizontal.png';
+import icon_user from '../img/user.png';
 
 function Header() {
   return (
     <header className="app-header">
-      <Link to="/">
-        <img src="/home-icon.png" className="home-icon" /> ICONE HOME AQUI
-      </Link>
+      <img src={logo} className="logo" alt="Logo Checkfy" />
+      <div className="container">
+        <div className='links-header'>
+          <Link to="/"><a href="../pages/Home.js" className="link-header">Home</a></Link>
+          <Link to="/"><a href="../pages/Sobre.js" className="link-header">Sobre</a></Link>
+          <Link to="/"><a href="../pages/Documentos.js" className="link-header">Documentos</a></Link>
+        </div>
+        <a href="../pages/Perfil.js">
+          <img  src={icon_user} className="icon-user" alt="Usuário" />
+        </a>
+      </div>
     </header>
   );
 }
