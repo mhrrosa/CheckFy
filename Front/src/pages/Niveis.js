0902619fdf675e69f5e6b0820383c5b3b7b98128
mhrrosa <71407848+mhrrosa@users.noreply.github.com>
2024-05-16 19:01:12 -0300

    merge main

--- Front/src/pages/Niveis.js
+++ Front/src/pages/Niveis.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { getNiveis, createNivel, updateNivel, deleteNivel } from '../services/Api';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Niveis.css';
+import logo from '../img/logo_horizontal.png';
 
 function Niveis() {
   const [niveis, setNiveis] = useState([]);
   const [novoNivel, setNovoNivel] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     carregarNiveis();
@@ -52,6 +55,7 @@ function Niveis() {
 
   return (
     <div className="niveis-container">
+<<<<<<< HEAD
       <h1>Gerenciamento de Níveis</h1>
       <input
         type="text"
@@ -85,13 +89,60 @@ function Niveis() {
                   <button onClick={() => atualizarNivel(nivel.id, nivel.nivel)}>Atualizar</button>
                   <button onClick={() => removerNivel(nivel.id)}>Remover</button>
                 </td>
+=======
+      <div className="form-section">
+        <button className="close-button" onClick={() => navigate('/modelo')}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </button>
+        <h1 className='level-management-title'>GERENCIAMENTO DE NÍVEIS</h1>
+        <div className="input-button">
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Novo Nível"
+            value={novoNivel}
+            onChange={(e) => setNovoNivel(e.target.value)}
+          />
+          <button className='button-add' onClick={adicionarNivel}>ADICIONAR</button>
+        </div>
+        <img src={logo} className="logo-form" alt="Logo Checkfy" />
+        {niveis.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Nível</th>
+                <th>Ações</th>
+>>>>>>> origin/DEV_Front
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>Nenhum nível encontrado.</p>
-      )}
+            </thead>
+            <tbody>
+              {niveis.map(nivel => (
+                <tr key={nivel.id}>
+                  <td>
+                    <input
+                      type="text"
+                      value={nivel.nivel}
+                      onChange={(e) => {
+                        const valorAtualizado = e.target.value;
+                        setNiveis(niveis.map(n => (n.id === nivel.id ? { ...n, nivel: valorAtualizado } : n)));
+                      }}
+                    />
+                  </td>
+                  <td>
+                    <button onClick={() => atualizarNivel(nivel.id, nivel.nivel)}>Atualizar</button> {/* Novo botão */}
+                    <button onClick={() => removerNivel(nivel.id)}>Remover</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Nenhum nível encontrado.</p>
+        )}
+      </div>
     </div>
   );
 }
