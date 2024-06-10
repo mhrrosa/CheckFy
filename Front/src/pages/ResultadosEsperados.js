1d8dca2ee3517e980608773a0335d6e143008075
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2024-06-09 22:23:49 -0300

    Atualização de navegação

--- Front/src/pages/ResultadosEsperados.js
+++ Front/src/pages/ResultadosEsperados.js
@@ -107,7 +107,7 @@ function ResultadosEsperados() {
   return (
     <div className="resultados-esperados-container">
       <div className='form-section resultados-esperados-form'>
-        <button className="close-button" onClick={() => navigate('/')}>
+        <button className="close-button" onClick={() => navigate('/modelo')}>
           <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <line x1="18" y1="6" x2="6" y2="18"></line>
             <line x1="6" y1="6" x2="18" y2="18"></line>
