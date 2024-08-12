4dc2b0e8a18601e725d4d1da2db9118da014b27b
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2024-08-12 19:31:49 -0300

    .

--- Front/src/index.js
+++ Front/src/index.js
@@ -4,13 +4,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <UserProvider>
       <App />
     </UserProvider>
-=======
-    <App />
->>>>>>> parent of 7ee08dab (Funcionalidade de controle de usuário)
   </React.StrictMode>,
   document.getElementById('root')
 );
