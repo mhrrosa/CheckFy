4d28b26d8b7dacfc3d2818fad36a26dd793bc06e
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2024-08-12 19:10:29 -0300

    .

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './pages/LoginCadastro';
@@ -10,7 +11,6 @@ import Modelo from './pages/Modelo';
 import Niveis from './pages/Niveis';
 import Processos from './pages/Processos';
 import ResultadosEsperados from './pages/ResultadosEsperados';
-import './App.css';
 import GerenciamentoVersaoModelo from './pages/GerenciamentoVersaoModelo';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -38,11 +38,11 @@ function AppContent() {
           <Route path="/evaluation" element={<Evaluation />} />
           <Route path="/update-evaluation" element={<UpdateEvaluation />} />
           <Route path="/results" element={<Results />} />
-          <Route path="/modelo/:ano" element={<Modelo />} />
+          <Route path="/modelo" element={<Modelo />} />
           <Route path="/gerenciamento-versao-modelo" element={<GerenciamentoVersaoModelo />} />
-          <Route path="/niveis/:ano" element={<Niveis />} />
-          <Route path="/processos/:ano" element={<Processos />} />
-          <Route path="/resultados-esperados/:ano" element={<ResultadosEsperados />} />
+          <Route path="/niveis" element={<Niveis />} />
+          <Route path="/processos" element={<Processos />} />
+          <Route path="/resultados-esperados" element={<ResultadosEsperados />} />
         </Routes>
       </div>
       {!isLoginRoute && <Footer />}
