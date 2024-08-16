2646aece866cf21b291918007af438bd62313777
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2024-08-16 19:03:32 -0300

    Revert "!!GitHub_Desktop<main>"
    This reverts commit 0343176123e82c11b40d73b0bfd2faf7d955418a.

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -9,35 +10,34 @@ import UpdateEvaluation from './pages/UpdateEvaluation';
 import Results from './pages/Results';
 import LoginCadastro from './pages/LoginCadastro';
 import Gerenciamento from './pages/Gerenciamento';
-import GerenciamentoVersaoModelo from './pages/GerenciamentoVersaoModelo';
 import Niveis from './pages/Niveis';
 import Processos from './pages/Processos';
 import ResultadosEsperados from './pages/ResultadosEsperados';
+import GerenciamentoVersaoModelo from './pages/GerenciamentoVersaoModelo';
 
 function App() {
-  const location = useLocation();
-  const isLoginCadastro = location.pathname === '/login-cadastro';
-
   return (
-    <div id="app-container">
-      {!isLoginCadastro && <Header />}
-      <div id="content-wrap">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-evaluation" element={<CreateEvaluation />} />
-          <Route path="/evaluation" element={<Evaluation />} />
-          <Route path="/update-evaluation" element={<UpdateEvaluation />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/login-cadastro" element={<LoginCadastro />} />
-          <Route path="/gerenciamento" element={<Gerenciamento />} />
-          <Route path="/gerenciamento-versao-modelo" element={<GerenciamentoVersaoModelo />} />
-          <Route path="/niveis" element={<Niveis />} />
-          <Route path="/processos" element={<Processos />} />
-          <Route path="/resultados-esperados" element={<ResultadosEsperados />} />
-        </Routes>
+    <Router>
+      <div id="app-container">
+        <Header />
+        <div id="content-wrap">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-evaluation" element={<CreateEvaluation />} />
+            <Route path="/evaluation" element={<Evaluation />} />
+            <Route path="/update-evaluation" element={<UpdateEvaluation />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/login-cadastro" element={<LoginCadastro />} />
+            <Route path="/gerenciamento" element={<Gerenciamento />} />
+            <Route path="/gerenciamento-versao-modelo" element={<GerenciamentoVersaoModelo />} />
+            <Route path="/niveis" element={<Niveis />} />
+            <Route path="/processos" element={<Processos />} />
+            <Route path="/resultados-esperados" element={<ResultadosEsperados />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      {!isLoginCadastro && <Footer />}
-    </div>
+    </Router>
   );
 }
 
--- Front/src/index.js
+++ Front/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Importe o BrowserRouter
 import App from './App';
 import { UserProvider } from './contexts/UserContext'; // Importe o UserProvider
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Envolva o App com o BrowserRouter */}
-      <UserProvider> {/* Envolva o App com o UserProvider */}
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    <UserProvider> {/* Envolva o App com o UserProvider */}
+      <App />
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
