852d0e0561de6b61ee625b99c87e35e864443b95
joaowozniack <90704865+joaowozniack@users.noreply.github.com>
2024-08-20 19:12:16 -0300

    ajuste no botão de adicionar email

--- Front/src/pages/CreateEvaluation.js
+++ Front/src/pages/CreateEvaluation.js
@@ -159,7 +159,7 @@ function CreateEvaluation() {
                 <button className="button-add-email" type="button" onClick={() => addEmail(setAdjuntoEmails)}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="12" y1="8" x2="12" y1="16"></line>
+                    <line x1="12" y1="8" x2="12" y2="16"></line>
                     <line x1="8" y1="12" x2="16" y2="12"></line>
                   </svg>
                 </button>
@@ -183,7 +183,7 @@ function CreateEvaluation() {
                 <button className="button-add-email" type="button" onClick={() => addEmail(setColaboradorEmails)}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="12" y1="8" x2="12" y1="16"></line>
+                    <line x1="12" y1="8" x2="12" y2="16"></line>
                     <line x1="8" y1="12" x2="16" y2="12"></line>
                   </svg>
                 </button>
