85019892395c70a350f22e49908b4b35247ab59f
mhrrosa <71407848+mhrrosa@users.noreply.github.com>
2024-09-03 14:08:33 -0300

    email auditor

--- back/Email.py
+++ back/Email.py
@@ -88,3 +88,67 @@ class Email:
         except Exception as e:
             print(f"Erro ao buscar avaliação no banco de dados: {e}")
             raise e
+    
+
+    def enviar_email_auditor_avaliacao_inicial(self, id_avaliacao):
+        query = """
+            SELECT a.ID, a.Nome, a.Descricao, a.ID_Avaliador_Lider, u.Nome, 
+                   a.Status, atv.Descricao, a.ID_Empresa, e.Nome, n.Nivel, 
+                   v.Nome, a.ID_Instituicao, a.Atividade_Planejamento, 
+                   a.Cronograma_Planejamento, a.Avaliacao_Aprovada_Pela_Softex,
+                   a.ID_Atividade, a.ID_Nivel_Solicitado, a.ID_Versao_Modelo
+            FROM avaliacao a
+            LEFT JOIN empresa e ON a.ID_Empresa = e.ID
+            LEFT JOIN nivel_maturidade_mpsbr n ON a.ID_Nivel_Solicitado = n.ID
+            LEFT JOIN usuario u ON a.ID_Avaliador_Lider = u.ID
+            LEFT JOIN versao_modelo v ON a.ID_Versao_Modelo = v.ID
+            LEFT JOIN atividade atv ON a.ID_Atividade = atv.ID
+            WHERE a.ID = %s
+        """
+
+        try:
+            self.db.cursor.execute(query, (id_avaliacao,))
+            row = self.db.cursor.fetchone()
+
+            if row:
+                # Extraindo dados da avaliação
+                id = row[0]
+                nome = row[1]
+
+                # Configurando o e-mail
+                remetente = "checkfy123@gmail.com"
+                destinatario = "checkfy123@gmail.com"
+                assunto = f"Informações da Avaliação Inicial - ID {id}"
+                
+                corpo = f"""
+                Prezado(a) Auditor(a),
+
+                Seguem as informações da avaliação inicial da Avaliação ID {id} para a realização da auditoria:
+
+                falta relatorio de ajuste
+
+                Atenciosamente,
+                Equipe de Avaliação
+                """
+
+                # Criando a mensagem de e-mail
+                mensagem = MIMEMultipart()
+                mensagem['From'] = remetente
+                mensagem['To'] = destinatario
+                mensagem['Subject'] = assunto
+                mensagem.attach(MIMEText(corpo, 'plain'))
+
+                # Enviando o e-mail via servidor SMTP do Gmail
+                try:
+                    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+                        server.starttls()
+                        server.login(remetente, "bsvsordretbmoatk")
+                        server.send_message(mensagem)
+                    print("E-mail enviado com sucesso!")
+                except Exception as e:
+                    print(f"Erro ao enviar e-mail: {e}")
+            else:
+                print('Avaliação não encontrada')
+        except Exception as e:
+            print(f"Erro ao buscar avaliação no banco de dados: {e}")
+            raise e
