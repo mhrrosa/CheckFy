512163fd1c8e2dbeed6a80e3d03997ddf8982922
Arthur Prezotto Andrade <70661461+arthurprezottoandrade@users.noreply.github.com>
2024-10-03 20:17:28 -0300

    Conclusão da capacidade de processo
    Conclusão dessa página, da auditoria e do ajuste da auditoria dessa parte

--- back/ImplementacaoOrganizacional.py
+++ back/ImplementacaoOrganizacional.py
@@ -26,3 +26,67 @@ class ImplementacaoOrganizacional:
         except Exception as e:
             print(f"Erro ao buscar perguntas de implementação: {e}")
             raise
+
+    def add_capacidade_processo_organizacional(self, valores):
+        cursor = self.db.conn.cursor(dictionary=True)
+        query = """
+        INSERT INTO nota_capacidade_processo_organizacional 
+        (ID_Avaliacao, ID_Processo, Nota) 
+        VALUES (%s, %s, %s)
+        """
+        try:
+            # Executa a inserção de múltiplos valores ao mesmo tempo
+            cursor.executemany(query, valores)
+            self.db.conn.commit()  # Confirma a operação no banco de dados
+        except Exception as e:
+            self.db.conn.rollback()  # Desfaz a transação em caso de erro
+            print(f"Erro ao adicionar os graus de implementação: {e}")
+            raise
+        finally:
+            cursor.close()  # Garante que o cursor será fechado
+
+
+    def get_capacidade_processo_organizacional(self, id_avaliacao):
+        cursor = self.db.conn.cursor(dictionary=True)
+        try:
+            query_graus = "SELECT * FROM nota_capacidade_processo_organizacional WHERE ID_Avaliacao = %s"
+            cursor.execute(query_graus, (id_avaliacao,))
+            graus = cursor.fetchall()
+
+            # Verificar se a consulta retornou algo
+            if not graus:
+                return None  # Retorna uma mensagem informando que não encontrou nada
+            
+            capacidade_processo = []
+            for grau in graus:
+                grau_dict = {
+                    'ID': grau['ID'],
+                    'ID_Processo': grau['ID_Processo'],
+                    'Nota': grau['Nota'],
+                    'ID_Avaliacao': grau['ID_Avaliacao'],
+                }
+                capacidade_processo.append(grau_dict)
+            cursor.close()
+            return capacidade_processo
+
+        except Exception as e:
+            print(f"Erro ao buscar graus de implementação: {e}")
+            raise
+
+    def update_capacidade_processo_organizacional_batch(self, update_data):
+        cursor = self.db.conn.cursor(dictionary=True)
+        query = """
+            UPDATE nota_capacidade_processo_organizacional
+            SET Nota = %s
+            WHERE ID_Avaliacao = %s AND ID_Processo = %s
+        """
+        try:
+            # Executa a atualização para cada item no batch de dados
+            cursor.executemany(query, update_data)
+            self.db.conn.commit()  # Confirma a operação no banco de dados
+        except Exception as e:
+            self.db.conn.rollback()  # Desfaz a transação em caso de erro
+            print(f"Erro ao atualizar capacidade do processo: {e}")
+            raise
+        finally:
+            cursor.close()  # Garante que o cursor será fechado
--- back/ImplementacaoProjeto.py
+++ back/ImplementacaoProjeto.py
@@ -26,3 +26,67 @@ class ImplementacaoProjeto:
         except Exception as e:
             print(f"Erro ao buscar perguntas de implementação: {e}")
             raise
+
+    def add_capacidade_processo_projeto(self, valores):
+        cursor = self.db.conn.cursor(dictionary=True)
+        query = """
+        INSERT INTO nota_capacidade_processo_projeto
+        (ID_Avaliacao, ID_Projeto, Nota) 
+        VALUES (%s, %s, %s)
+        """
+        try:
+            # Executa a inserção de múltiplos valores ao mesmo tempo
+            cursor.executemany(query, valores)
+            self.db.conn.commit()  # Confirma a operação no banco de dados
+        except Exception as e:
+            self.db.conn.rollback()  # Desfaz a transação em caso de erro
+            print(f"Erro ao adicionar os graus de implementação: {e}")
+            raise
+        finally:
+            cursor.close()  # Garante que o cursor será fechado
+
+
+    def get_capacidade_processo_projeto(self, id_avaliacao):
+        cursor = self.db.conn.cursor(dictionary=True)
+        try:
+            query_graus = "SELECT * FROM nota_capacidade_processo_projeto WHERE ID_Avaliacao = %s"
+            cursor.execute(query_graus, (id_avaliacao,))
+            graus = cursor.fetchall()
+
+            # Verificar se a consulta retornou algo
+            if not graus:
+                return None  # Retorna uma mensagem informando que não encontrou nada
+            
+            capacidade_processo = []
+            for grau in graus:
+                grau_dict = {
+                    'ID': grau['ID'],
+                    'ID_Projeto': grau['ID_Projeto'],
+                    'Nota': grau['Nota'],
+                    'ID_Avaliacao': grau['ID_Avaliacao'],
+                }
+                capacidade_processo.append(grau_dict)
+            cursor.close()
+            return capacidade_processo
+
+        except Exception as e:
+            print(f"Erro ao buscar graus de implementação: {e}")
+            raise
+
+    def update_capacidade_processo_projeto_batch(self, update_data):
+        cursor = self.db.conn.cursor(dictionary=True)
+        query = """
+            UPDATE nota_capacidade_processo_projeto
+            SET Nota = %s
+            WHERE ID_Avaliacao = %s AND ID_Projeto = %s
+        """
+        try:
+            # Executa a atualização para cada item no batch de dados
+            cursor.executemany(query, update_data)
+            self.db.conn.commit()  # Confirma a operação no banco de dados
+        except Exception as e:
+            self.db.conn.rollback()  # Desfaz a transação em caso de erro
+            print(f"Erro ao atualizar capacidade do processo: {e}")
+            raise
+        finally:
+            cursor.close()  # Garante que o cursor será fechado
